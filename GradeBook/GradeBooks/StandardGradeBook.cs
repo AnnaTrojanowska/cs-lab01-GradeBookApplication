524840500d288d460599c5df09fcc6df460973b7
Vinan x <a.trojanowska3291@gmail.com>
2022-03-26 17:24:48 +0100

    test 3 i 4

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+        
+        public StandardGradeBook(string name) : base(name)
+        {
+        }
+    }
+
+
+    
+}
