517897f91869ae1f30f8d7a0c9aa39ec4bc773bd
Vinan x <a.trojanowska3291@gmail.com>
2022-03-28 21:14:16 +0200

    test 9/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,8 +6,30 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        
         public RankedGradeBook(string name) : base(name)
         {
+            
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+           if (Students.Count <5)
+            {
+                throw new InvalidOperationException("Students number less than 5");
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade < 60)
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade < 40)
+                return 'D';
+            else
+                return 'F';
+        }
+        
+
+        
     }
 }
